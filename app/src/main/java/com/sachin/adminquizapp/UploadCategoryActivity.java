2126ed4baa1f5482f47d31a2ee2367f2eef3bbbf
sachin kumar <allknowledge34@gmail.com>
2025-04-15 09:19:31 +0530

    Initial commit

---
+++ app/src/main/java/com/sachin/adminquizapp/UploadCategoryActivity.java
@@ -0,0 +1,19 @@
+package com.sachin.adminquizapp;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class UploadCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_upload_category);
+    }
+}
